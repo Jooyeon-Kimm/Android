1197c36fe696e8907d62121ff8931d89bf623966
Jooyeon-Kimm <jooooyeonkim@gmail.com>
2025-06-27 17:12:08 +0900

    자료 업로드

---
+++ Part4/Chapter4/15_ToDo/app/src/main/java/com/example/todo/MainActivity.kt
@@ -0,0 +1,312 @@
+package com.example.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todo.ui.theme.ToDoTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    // {onCreate}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopLevel()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val toDoList = remember { mutableStateListOf<ToDoData>() }
+    // MutableStateList가 추가, 삭제, 변경되었을 때만 UI 갱신
+    // 항목 하나의 값을 바꾸는 것보다 항목 자체를 바꾸는게
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    val onSubmit : (String) -> Unit = {
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        toDoList.add(ToDoData(key, text))
+        setText("")
+    }
+
+    val onToggle : (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onDelete : (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
+    }
+
+    val onEdit : (Int, String) -> Unit = { key, text ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = text)
+    }
+
+    // STEP 4) `onSubmit`, `onEdit`, `onToggle`, `onDelete`를 만들어
+    // `ToDo`에 연결합니다.
+
+    Scaffold (
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            ToDoInput(
+                text = text,
+                onTextChange = setText,
+                onSubmit = onSubmit,
+                snackbarHostState = snackbarHostState,
+                coroutineScope = coroutineScope
+            )
+
+
+            // STEP 3) `LazyColumn`으로 `toDoList`를 표시합시다.
+            // `key`를 `toDoData`의 `key`를 사용합니다.
+            LazyColumn {
+                items(toDoList, key = { it.key }) {toDoData ->
+                    ToDo(
+                        toDoData = toDoData,
+                        onToggle = onToggle,
+                        onDelete = onDelete,
+                        onEdit = onEdit
+                    )
+                }
+            }
+
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToDoTheme {
+        TopLevel()
+    }
+}
+
+@Composable
+fun ToDoInput(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSubmit: (String) -> Unit,
+    snackbarHostState :SnackbarHostState,
+    coroutineScope : CoroutineScope
+) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f),
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Button(onClick = {
+            if (text.isNotEmpty()) {
+                onSubmit(text)
+            } else {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar("텍스트를 입력해주세요")
+                    return@launch
+                }
+            }
+        }) {
+            Text("입력")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoInputPreview() {
+    ToDoTheme {
+        val snackbarHostState = remember { SnackbarHostState() }
+        val coroutineScope = rememberCoroutineScope()
+        ToDoInput("테스트", {}, {}, snackbarHostState, coroutineScope)
+    }
+}
+
+@Composable
+fun ToDo(
+    toDoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = { _, _ -> },
+    onToggle: (key: Int, checked: Boolean) -> Unit = { _, _ -> },
+    onDelete: (key: Int) -> Unit = {}
+) {
+    var isEditing by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp)
+    ) {
+
+        // STEP 1) `Row`를 만들고 `toDoData.text`를 출력하고
+        // 완료를 체크하는 체크박스, 수정 버튼, 삭제 버튼을 만드세요.
+//        Row (
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Text(
+//                modifier = Modifier.weight(1f),
+//                text = toDoData.text
+//            )
+//            Text(
+//                text = "완료"
+//            )
+//            Checkbox(
+//                checked = toDoData.done,
+//                onCheckedChange = { checked ->
+//                    onToggle(toDoData.key, checked)
+//                }
+//            )
+//            Button(
+//                onClick = {}
+//            ) {
+//                Text(
+//                    text = "수정"
+//                )
+//            }
+//            Spacer(
+//                modifier = Modifier.size(4.dp)
+//            )
+//            Button(
+//                onClick = {}
+//            ) {
+//                Text(
+//                    text = "삭제"
+//                )
+//            }
+//        }
+
+        // STEP 2) `Crossfade`를 통해 `isEditing`을 따라 다른 UI를 보여줍니다.
+        // `OutlinedTextField`와 `Button을 넣어봅시다.
+        /*
+            Crossfade란
+            "A 상태 → B 상태"로 바뀔 때,
+            갑자기 바뀌지 않고 서서히 흐려지며 전환되도록 해주는 Compose 애니메이션 함수입니다.
+         */
+        Crossfade(
+            targetState = isEditing
+        ) {
+            when (it) {
+                false -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = toDoData.text
+                        )
+                        Text(
+                            text = "완료"
+                        )
+                        Checkbox(
+                            checked = toDoData.done,
+                            onCheckedChange = { checked ->
+                                onToggle(toDoData.key, checked)
+                            }
+                        )
+                        Button(
+                            onClick = {
+                                isEditing = true
+                            }
+                        ) {
+                            Text(
+                                text = "수정"
+                            )
+                        }
+                        Spacer(
+                            modifier = Modifier.size(4.dp)
+                        )
+                        Button(
+                            onClick = {
+                                onDelete(toDoData.key)
+                            }
+                        ) {
+                            Text(
+                                text = "삭제"
+                            )
+                        }
+                    }
+                }
+
+                true -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    {
+                        var (newText, setNewText) = remember { mutableStateOf(toDoData.text) }
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setNewText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.padding(4.dp))
+                        Button(
+                            onClick = {
+                                onEdit(toDoData.key, newText)
+                                isEditing = false
+                            }
+                        ) {
+                            Text("완료")
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoPreview() {
+    ToDoTheme {
+        ToDo(ToDoData(1, "nice", true))
+    }
+}
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
